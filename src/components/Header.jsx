0294b0de2ea8c5a1d3b65419698d6373c2d37606
elianabcrr <eliana.becerra0916@gmail.com>
2022-04-22 15:17:17 -0400

    [update] url logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React, {useState, useContext} from 'react';
 import '@styles/Header.scss';
 import Menu from '@components/DesktopMenu';
 import menu from '@icons/icon_menu.svg';
-import logoSite from '@logos/logo_bota_fora.svg';
+import logoSite from '@logos/logo_y.svg';
 import shoppingCartIcon from '@icons/icon_shopping_cart.svg';
 import AppContext from '@context/AppContext';
 import ShoppingCart from '@containers/ShoppingCart';
