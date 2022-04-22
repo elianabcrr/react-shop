a57be61a894a6a3ac9b22df48d791ecec2609cb8
elianabcrr <eliana.becerra0916@gmail.com>
2022-04-22 15:29:17 -0400

    [update] url logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import React, {useState, useContext} from 'react';
 import '@styles/Header.scss';
 import Menu from '@components/DesktopMenu';
 import menu from '@icons/icon_menu.svg';
+// import logoSite from '@logos/logo_yard_sale.svg';
 import logoSite from '@logos/logo_yard_sale.png';
 import shoppingCartIcon from '@icons/icon_shopping_cart.svg';
 import AppContext from '@context/AppContext';
